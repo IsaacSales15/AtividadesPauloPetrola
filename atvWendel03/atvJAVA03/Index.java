399d12713b37b4e92901ef1e1c1f2a54afcdd2bb
Isaac Sales Rodrigues <124395470+IsaacSales15@users.noreply.github.com>
2024-03-13 23:16:00 -0300

    E é né

--- atvWendel03/atvJAVA03/Index.java
+++ atvWendel03/atvJAVA03/Index.java
@@ -3,6 +3,8 @@ public class Index {
     public static void main(String[] args) {
         Script script = new Script();
         //script.ViewNumbers();
-        script.InvertNumbers();
+        //script.InvertNumbers();
+        //script.Wowels();
+        script.ViewAverage();
     }
 }
